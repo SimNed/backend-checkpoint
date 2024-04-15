90caf283423a0c64db00046812601a01dc807e89
SimNed <simon.nedjari@outlook.com>
2024-04-15 15:46:45 +0200

    add index and main func

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { buildSchema } from "type-graphql";
+import { getDataSource } from "./database/database";
+import { CountryResolver } from "./resolvers/CountryResolver";
+
+const PORT = 4000;
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [CountryResolver],
+    validate: true,
+  });
+  const server = new ApolloServer({ schema });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: PORT },
+  });
+
+  await getDataSource();
+
+  console.log(`🚀  Server ready at: ${url}`);
+};
+
+main();
